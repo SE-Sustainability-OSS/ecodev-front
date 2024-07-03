6bc19cc92c5366d336de4b7c5c87d07be8812804
Amaury Salles <amaury.salles@eviden.com>
2024-07-03 14:23:45 +0200

    removed folder structure

--- ecodev_front/__init__.py
+++ ecodev_front/__init__.py
@@ -1,3 +1,9 @@
+from ecodev_front.app_layout import dash_base_layout
+from ecodev_front.card import background_card
+from ecodev_front.card import card_section
+from ecodev_front.card import card_title
+from ecodev_front.card import kpi
+from ecodev_front.card import macro_info
 from ecodev_front.constants import CHILDREN
 from ecodev_front.constants import DATA
 from ecodev_front.constants import DISABLED
@@ -5,6 +11,15 @@ from ecodev_front.constants import N_CLICKS
 from ecodev_front.constants import PATHNAME
 from ecodev_front.constants import STYLE
 from ecodev_front.constants import VALUE
+from ecodev_front.data_table import data_table
+from ecodev_front.display_utils import get_magnitude
+from ecodev_front.display_utils import number_formatting
+from ecodev_front.div import centered_div
+from ecodev_front.divider import divider
+from ecodev_front.footer import app_footer
+from ecodev_front.footer import footer_style
+from ecodev_front.graph import graph_box
+from ecodev_front.group import group
 from ecodev_front.ids import APPSHELL
 from ecodev_front.ids import ASIDE
 from ecodev_front.ids import FOOTER_ID
@@ -20,13 +35,39 @@ from ecodev_front.ids import PAGE
 from ecodev_front.ids import RIGHT_ASIDE_ID
 from ecodev_front.ids import TOKEN
 from ecodev_front.ids import URL
-from ecodev_front.layouts import dash_base_layout
-
+from ecodev_front.login import login
+from ecodev_front.nav_header import app_header
+from ecodev_front.nav_header import app_logo
+from ecodev_front.nav_header import app_title
+from ecodev_front.nav_header import header_divider
+from ecodev_front.nav_items import action_item
+from ecodev_front.nav_items import menu
+from ecodev_front.nav_items import menu_item
+from ecodev_front.navlink import navlink
+from ecodev_front.pie_chart import pie_chart
+from ecodev_front.report_value import report_value
+from ecodev_front.search_bar import search_bar
+from ecodev_front.segment_control import segmented_control
+from ecodev_front.segment_control import segmented_control_label
+from ecodev_front.select import select
+from ecodev_front.select import select_label
+from ecodev_front.stack import stack
+from ecodev_front.stepper import stepper_step
+from ecodev_front.stepper import vertical_stepper
+from ecodev_front.text import sub_text
+from ecodev_front.text import text_header
+from ecodev_front.upload_box import upload_box
 
 __all__ = [
-    'dash_base_layout',
     'CHILDREN', 'DATA', 'PATHNAME', 'N_CLICKS', 'VALUE', 'URL', 'TOKEN',
     'NAVBAR_ID', 'LOGIN_USERNAME_INPUT_ID', 'LOGIN_PASSWORD_INPUT_ID', 'LOGOUT_BTN_ID',
     'LOGIN_BTN_ID', 'FOOTER_ID', 'MAIN_CONTENT_ID', 'LEFT_ASIDE_ID', 'RIGHT_ASIDE_ID',
-    'DISABLED', 'STYLE', 'PAGE', 'APPSHELL', 'ASIDE', 'NAVBAR'
+    'DISABLED', 'STYLE', 'PAGE', 'APPSHELL', 'ASIDE', 'NAVBAR', 'dash_base_layout',
+    'app_logo', 'app_title', 'app_header', 'menu', 'data_table', 'header_divider',
+    'action_item', 'login', 'menu_item', 'upload_box', 'card_section',
+    'card_title', 'macro_info', 'number_formatting', 'background_card', 'search_bar',
+    'graph_box', 'footer_style', 'report_value', 'centered_div', 'pie_chart',
+    'app_footer', 'vertical_stepper', 'stepper_step', 'group', 'segmented_control',
+    'select', 'stack', 'text_header', 'sub_text', 'divider', 'kpi',
+    'segmented_control_label', 'select_label', 'navlink', 'get_magnitude'
 ]
