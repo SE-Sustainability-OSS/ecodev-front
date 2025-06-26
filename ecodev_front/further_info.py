73ee5910ea32c6adc1fc0829e72efc2104df7427
trolland-se <thibault.rolland@se.com>
2025-06-26 12:15:14 +0200

    Update further_info.py

--- ecodev_front/further_info.py
+++ ecodev_front/further_info.py
@@ -2,22 +2,30 @@ import dash_mantine_components as dmc
 from dash_iconify import DashIconify
 
 
-def further_info(info: str | list[dmc.Text], large: bool = False) -> dmc.Tooltip:
+def further_info(
+    info: str | list[dmc.Text], 
+    large: bool = False, 
+    **kwargs
+) -> dmc.Tooltip:
     """
     Renders a gray info icon which opens the further info tooltip.
     """
+    kwargs.setdefault("position", "top-right")
+    kwargs.setdefault("multiline", True)
+    kwargs.setdefault("offset", 3)
+    kwargs.setdefault("radius", "md")
+    kwargs.setdefault("closeDelay", 300)
+    kwargs.setdefault("color", "white")
+    kwargs.setdefault("style", {"max-width": 500, "border": "1px solid #dcdcdc"})
+
     return dmc.Tooltip(
-        label=info,
-        position='top-right',
-        multiline=True,
-        offset=3,
-        radius='md',
-        color='white',
-        style={'max-width': 500, 'border': '1px solid #dcdcdc'},
-        closeDelay=300,
+        label=dmc.Text(info, c='black') if isinstance(info, str) else info,
         children=[
-            DashIconify(icon='material-symbols:info-outline-rounded',
-                        color='gray',
-                        width=28 if large else 22)
-        ]
+            DashIconify(
+                icon="material-symbols:info-outline-rounded",
+                color="gray",
+                width=28 if large else 22,
+            )
+        ],
+        **kwargs
     )
